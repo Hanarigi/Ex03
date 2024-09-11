89fb7914cd53bdeb3995d2b1be419f07ca7d777a
hana <hannaneh.rigi@gmail.com>
2024-09-11 17:17:50 +0330

    tamrin 1

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,13 @@
-// write your func here
+function reverseString(inputstring){
+    if (!inputstring === null){
+        let arr = [];
+    arr = inputstring;
+    let reversed = [];
+    for(let i = arr.length - 1; i>=0; i--){
+        reversed.push(arr[i]);
+    }
+    inputstring = reversed.join("");
+    return inputstring;
+    } 
+    else return "wrong input";
+}
