2edc06b19f5844a4bba5aa375ccfdd176f84ab15
hana <hannaneh.rigi@gmail.com>
2024-09-11 17:19:20 +0330

    tamrin 2

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,22 @@
-// write your func here
+function reverseString(inputstring){
+    if (!inputstring === null){
+        let arr = [];
+    arr = inputstring;
+    let reversed = [];
+    for(let i = arr.length - 1; i>=0; i--){
+        reversed.push(arr[i]);
+    }
+    inputstring = reversed.join("");
+    return inputstring;
+    } 
+    else return "wrong input";
+}
+function isPalindrome(str){
+    if(!str === null){
+        str = str.toLowerCase();
+    reversed = reverseString(str);
+    if (reversed === str) return true;
+    else return false;
+    }
+    else return "wrong input";
+}
