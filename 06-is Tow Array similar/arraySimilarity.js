23667d823b3a4186cb383a343355000823edcccc
hana <hannaneh.rigi@gmail.com>
2024-09-11 17:23:58 +0330

    tamrin 6

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,14 @@
-// write your func here
+function isTheSame(array1,array2){
+    if (array1 === null || array2 === null) return "wrong input";
+    else if(array1.length !== array2.length) return false;
+    else{
+        let check = 1;
+        for(let i =0; i < array1.length; i++){
+            if (array1[i] !== array2[i]) check = 0;
+            if (check === 0) break;
+        }
+        if (check === 1) return true;
+        else return false;
+    }
+}
+
