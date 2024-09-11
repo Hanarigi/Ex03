e3acd694c13160c979e36db584d9bd6eff1f2bba
hana <hannaneh.rigi@gmail.com>
2024-09-11 17:22:44 +0330

    tamrin 5

--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,7 @@
-// write your func here
+function maxValue(array) {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
